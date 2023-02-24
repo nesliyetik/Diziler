9b4ecdbac26e7522ddb96e3bdd5749207c4c3048
Nesli Yetik <snesliyetik@gmail.com>
2023-02-24 22:12:45 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dizi Tanımlama
+            string[] renkler = new string[5]; //5 tane elemanı olan string elemanlardan oluşan dizi tanımlamış olduk
+
+            string[] hayvanlar = {"Kedi","Köpek","Kuş","Maymun"}; 
+
+            int[] dizi;
+            dizi = new int[5];
+
+            //Dizilere Değer Atama ve Erişim
+
+            renkler [0] = "Mavi";
+
+            dizi[3] = 10;
+
+            Console.WriteLine(hayvanlar[1]);
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+            //Döngülerle Dizi Kullanımı
+            //klavyeden girilen n tane sayının ortalamasını alan program
+
+            Console.Write("Lütfen dizinin eleman sayısını giriniz: ");
+
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int[] sayiDizisi = new int[diziUzunlugu]; //console'dan girilen sayıya göre bir dizi aldık bellekten
+            
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.Write("Lütfen {0}. sayıyı giriniz: ", i+1);
+                sayiDizisi[i] =int.Parse(Console.ReadLine()); //consoledan aldığım değeri dizinin i. elemanına atıyorum
+                 
+            }
+            
+            int toplam = 0;
+            foreach (var sayi in sayiDizisi)
+    
+                toplam += sayi;
+
+            Console.WriteLine("Ortalama: " + toplam/diziUzunlugu );    
+
+
+        }
+
+    }
+
+}
